243ebc22696c803219fddc448472efc1e873570f
Billy Lam <billyl99@gmail.com>
2013-05-27 16:14:27 -0700

    Add youtube embed helper

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,2 +1,15 @@
 module VideosHelper
+  #given a youtube url
+  #regex replace to return an embed url
+  #"borrowed" from http://stackoverflow.com/questions/5909121/converting-a-regular-youtube-link-into-an-embedded-video
+  def url_embed(url)
+    #for youtu.be urls
+    if url[/youtu\.be\/([^\?]*)/]
+      id = $1
+    else
+      url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      id = $5
+    end
+      %Q{<iframe title="YouTube video player" width="560" height="315" src="http://www.youtube.com/embed/#{ id }" frameborder="0" allowfullscreen></iframe>}
+  end
 end
