127ce3dffd90c25fec4ee11d3aedc4d33f92bafe
Billy Lam <billyl99@gmail.com>
2013-05-29 20:51:47 -0700

    Requiring playlist to have a user

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,5 @@ class Playlist < ActiveRecord::Base
   has_many :pl_additions
   has_many :videos, through: :pl_additions
   belongs_to :user
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130530033457) do
+ActiveRecord::Schema.define(:version => 20130530034943) do
 
   create_table "pl_additions", :force => true do |t|
     t.integer  "playlist_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130530033457) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
