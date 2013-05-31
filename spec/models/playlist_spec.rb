21fdb738a8482c3e221e0a8131d1edcb1d11800e
Billy Lam <billyl99@gmail.com>
2013-05-30 17:32:13 -0700

    More model tests

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Playlist do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryGirl.create(:user)
+    @playlist = @user.playlists.build(name:"Playlist")
+  end
+
+  subject { @playlist }
+
+  it { should respond_to :name }
+  it { should respond_to :user_id }
+  it { should respond_to :user }
+  its(:user) { should == @user }
+
+  describe "when user_id is not present" do
+    before { @playlist.user_id = nil }
+    it { should_not be_valid }
+  end
 end
