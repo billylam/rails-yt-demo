4234bf66f42cfe4e2679a6b0f0898a8d3067af3a
Billy Lam <billyl99@gmail.com>
2013-05-30 19:09:57 -0700

    Switching playlist to render thumbnails

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,7 +2,11 @@ module VideosHelper
   #given a youtube url
   #regex replace to return an embed url
   #"borrowed" from http://stackoverflow.com/questions/5909121/converting-a-regular-youtube-link-into-an-embedded-video
-  def url_embed(youtube_id)
-      %Q{<iframe title="YouTube video player" width="560" height="315" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  def embed(youtube_id)
+      raw %Q{<iframe title="YouTube video player" width="560" height="315" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+  end
+
+  def get_thumbnail_for(youtube_id)
+    "http://img.youtube.com/vi/#{ youtube_id }/default.jpg"
   end
 end
