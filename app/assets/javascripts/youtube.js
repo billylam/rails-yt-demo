0f95be0ca05a8f81ac97d060facabf4f8fdc4d60
Billy Lam <billyl99@gmail.com>
2013-06-07 22:00:37 -0700

    Improve empty playlist functionality

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $("#playlist").on("click", ".playlistThumb", function(event){
-    // if reload is true, we need to call js loadplaylist first and set it to that position
+    // if reload is true, we need to immediately reload playlist 
     if (reload == true) {
       reloadPlaylist($(this).data('position'));
     }
