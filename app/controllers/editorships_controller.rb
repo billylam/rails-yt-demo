8a504435270f1b4ccd5afeac0d63a6482861be7a
Billy Lam <billyl99@gmail.com>
2013-06-18 22:48:06 -0700

    AJAX toggle for allow/disallow button

--- app/controllers/editorships_controller.rb
+++ app/controllers/editorships_controller.rb
@@ -1,13 +1,19 @@
 class EditorshipsController < ApplicationController
   def create
-    other_user = User.find(params[:editorship][:editor_id])
-    current_user.allow!(other_user)
-    redirect_to root_path
+    @other_user = User.find(params[:editorship][:editor_id])
+    current_user.allow!(@other_user)
+    respond_to do |format|
+      format.html { redirect_to index }
+      format.js
+    end
   end
 
   def destroy
-    other_user = Editorship.find(params[:id]).editor
-    current_user.disallow!(other_user)
-    redirect_to root_path
+    @other_user = Editorship.find(params[:id]).editor
+    current_user.disallow!(@other_user)
+    respond_to do |format|
+      format.html { redirect_to index }
+      format.js
+    end
   end
 end
